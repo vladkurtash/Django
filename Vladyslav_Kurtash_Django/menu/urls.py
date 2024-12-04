08fb999846163b6104aa0e0c3145f60d4332012b
vladkurtash <er.vladkurtash@gmail.com>
2024-12-04 23:53:42 +0200

    Add More Functionality

--- Vladyslav_Kurtash_Django/menu/urls.py
+++ Vladyslav_Kurtash_Django/menu/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('secondPage', views.second_page, name='secondPage'),
     path('create/', views.create_post, name='create_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
 ]
--- Vladyslav_Kurtash_Django/menu/views.py
+++ Vladyslav_Kurtash_Django/menu/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
-from .models import Post  # Імпортуйте вашу модель Post
+from .models import Post
 
 def home(request):
-    posts = Post.objects.all()  # Отримання всіх постів
+    posts = Post.objects.all()
     return render(request, 'home.html', {'posts': posts})
 
 def second_page(request):
@@ -13,10 +13,23 @@ def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()  # Зберігає пост в базу даних
-            return redirect('home')  # Перенаправлення на домашню сторінку
+            form.save()
+            return redirect('home')
     else:
         form = PostForm()
 
     return render(request, 'create_post.html', {'form': form})
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm(instance=post)
+    
+    return render(request, 'edit_post.html', {'form': form})
 
